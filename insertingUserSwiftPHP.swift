d92ae70b279964e4d377e18b4ce14c2d9edf3777
Luã Enrique Zangrande <lua.enriquezan@gmail.com>
2018-03-09 11:25:51 -0300

    Create insertingUserSwiftPHP.swift

---
+++ insertingUserSwiftPHP.swift
@@ -0,0 +1,77 @@
+//
+//  insertingUserSwiftPHP.swift
+//  insertingUserSwiftPHP
+//
+//  Created by Luã Enrique Zangrande on 09/03/2018.
+//  Copyright © 2018 lzstudios. All rights reserved.
+//
+
+import UIKit
+
+class insertingUserSwiftPHP: UIViewController{
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var nickText: UITextField!
+    @IBOutlet weak var senhaText: UITextField!
+    @IBOutlet weak var confSenhaText: UITextField!
+    @IBOutlet weak var dataNascimento: UIDatePicker!
+    
+    @IBAction func registerUser(_ sender: Any) {
+        let request = NSMutableURLRequest(url: NSURL(string: "www.yoursite.com/insertUser.php")! as URL) // setting up the url 
+        request.httpMethod = "POST" //defining the http method
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd" // formatting the date
+        let strDate = dateFormatter.string(from: NSDate() as Date)
+
+        let postString = "name=\(nameText.text!)&email=\(emailText.text!)&nick=\(nickText.text!)&pass=\(senhaText.text!)&dateb=\(strDate)" //setting up the request string
+        
+        
+        request.httpBody = postString.data(using: String.Encoding.utf8)
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            print("response = \(response)")
+            
+            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+        
+        
+        let alertController = UIAlertController(title: "User register", message:
+            "Added successfully", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil) //showing an alert controller 
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
